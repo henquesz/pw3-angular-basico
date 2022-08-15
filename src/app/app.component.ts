2438d67d6f5f18603d7aae8e229d16b4394f0c9f
Bruno e Marcos <BrunoEMarcos@test.com>
2022-08-15 08:54:50 -0300

    editor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'helloMundo';
+  title = 'Programação WEB III';
+  logo = "favicon.ico";
+  exibe = true;
+
+  exibirNome(){
+    return 'Bruno e Marcos';
+  }
+
+  exibirAlerta(){
+    alert("Hoje vou adotar um anao!");
+  }
+
+  hideInformacoes(){
+    if(this.exibe == false){
+      this.exibe = true;
+    }
+    else if(this.exibe == true){
+      this.exibe = false;
+    }
+  }
 }
